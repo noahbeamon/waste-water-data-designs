709268893d947924bf8e03a7a3b171109ad3c94c
Noah <noah.beamon648@gmail.com>
2021-01-10 22:11:31 -0500

    design updates

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react';
 import "./Footer.css"; 
 import { MdEmail } from "react-icons/md";
 import { AiFillTwitterSquare, AiFillLinkedin, AiFillGithub, AiFillPhone } from "react-icons/ai";
-import { SiGooglescholar, SiOrcid } from "react-icons/si";
+import { SiGooglescholar, SiOrcid, SiGithub } from "react-icons/si";
 import { CgWebsite } from "react-icons/cg";
+import dataScienceLogo from "./images/dataScienceLogo.jpg"; 
 
 const Footer = () => {
     return(
@@ -26,10 +27,26 @@ const Footer = () => {
                                 <a target="_blank" style={{margin: 10, color: 'rgba(85, 85, 85, 1)'}} href={"https://www.linkedin.com/in/heman-shakeri-61ba603a/"}>LinkedIn</a>
                             </li>
                             <li>
+                                <SiGithub size={20}/>
+                                <a target="_blank" style={{margin: 10, color: 'rgba(85, 85, 85, 1)'}} href={"https://github.com/hshakeri"}>Github</a>
+                            </li>
+                            <li>
+                                <SiGooglescholar size={20}/>
+                                <a target="_blank" style={{margin: 10, color: 'rgba(85, 85, 85, 1)'}} href={"https://scholar.google.com/citations?user=zFIIhGMAAAAJ&hl=en"}>Google Scholar</a>
+                            </li>
+                            <li>
+                                <SiOrcid size={20}/>
+                                <a target="_blank" style={{margin: 10, color: 'rgba(85, 85, 85, 1)'}} href={"https://orcid.org/0000-0002-9891-5748"}>ORCID</a>
+                            </li>
+                            <li>
                                 <CgWebsite size={20}/>
-                                <a target="_blank" style={{margin: 10, color: 'rgba(85, 85, 85, 1)'}} href={"https://hemanshakerilab.herokuapp.com/"}>Heman Shakeri Lab Website</a>
+                                <a target="_blank" style={{margin: 10, color: 'rgba(85, 85, 85, 1)'}} href={"https://hemanshakerilab.herokuapp.com/"}>Shakeri Lab Website</a>
                             </li>
                             <li style={{marginTop: 30}}>Site built by Noah Beamon</li>
+                            <li>
+                                <AiFillLinkedin size={20}/>
+                                <a target="_blank" style={{margin: 10, color: 'rgba(85, 85, 85, 1)'}} href={"https://www.linkedin.com/in/noah-b-8a45711b5/"}>Noah Beamon</a>
+                            </li>
                         </ul>
                     </div>
                     <div className="col">
@@ -52,14 +69,14 @@ const Footer = () => {
                     </div>
                     <div className="col">
                         <ul className="list-unstyled">
-                            <li><img src="https://brand.virginia.edu/sites/uva_brand/files/styles/embedded_image_smallest_1x/public/2017-08/rev_uva_logo_color3.png"/></li>
+                            <li><img style={{width: 375, height: 75}} src={dataScienceLogo}/></li>
                         </ul>
                     </div>
                 </div>
                 <hr />
                 <div className="row">
                     <p className="col-sm">
-                        &copy; {new Date().getFullYear()} University of Virginia Wastewater Tracker
+                        &copy; 2020-{new Date().getFullYear()} University of Virginia Wastewater Tracker
                     </p>
                 </div>
             </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,12 +5,16 @@ import {
     //BrowserRouter as Router,
     Link,
 } from 'react-router-dom'; 
+import rotunda from "./images/rotunda.png";
 
 function Navbar() {
   return (
     <div className="App">
         <ReactBootstrap.Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-        <ReactBootstrap.Navbar.Brand href="/"><strong style={{ color: '#2e3d82' }}>University of Virginia COVID-19 Wastewater Tracker</strong></ReactBootstrap.Navbar.Brand>
+        <ReactBootstrap.Navbar.Brand href="/">
+        <img style={{width: 30, height: 30, marginRight: 10}} src={rotunda} />
+          <strong style={{ color: '#2e3d82' }}>University of Virginia COVID-19 Wastewater Tracker</strong>
+        </ReactBootstrap.Navbar.Brand>
         <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
           <ReactBootstrap.Nav className="mr-auto">
@@ -31,6 +35,11 @@ function Navbar() {
             <Link to="/Faq" style={{textDecoration: "none"}}>
             <ReactBootstrap.Nav.Link eventKey={5} href="#faq">FAQ</ReactBootstrap.Nav.Link>
             </Link>
+            <p style={{margin: 8, color: '#2e3d82', cursor: "pointer"}}
+            onClick = {() => {
+              window.open("https://hemanshakerilab.herokuapp.com")
+            }}
+            ><strong>Lab Website</strong></p>
           </ReactBootstrap.Nav>
         </ReactBootstrap.Navbar.Collapse>
       </ReactBootstrap.Navbar>
